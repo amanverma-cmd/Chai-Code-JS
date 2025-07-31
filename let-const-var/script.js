a3a1c8d7c24347d8f8e2d51253e12a8dd25a0dfa
amanverma-cmd <aman.ver9345@gmail.com>
2025-07-31 22:12:45 +0530

    added chai-code-js files

---
+++ let-const-var/script.js
@@ -0,0 +1,14 @@
+const accountID = 144553;
+let accountEmail = "amaninfo@google.com";
+var accountPassword = "12345";
+accountCity = "Bihar";
+
+// accountID = 2;
+
+accountEmail = "aman.ver9345@gmail.com";
+accountPassword = "56789";
+accountCity = "Bengaluru";
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
