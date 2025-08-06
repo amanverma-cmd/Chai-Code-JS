b48d4f4802e3b81ea221a0de754568c85219e752
amanverma-cmd <aman.ver9345@gmail.com>
2025-08-06 09:35:51 +0530

    added the files

---
+++ datatypes-ecma/script.js
@@ -0,0 +1,26 @@
+"use strict"; //use js as newer version
+
+// alert("Hello"); //  we are using nodejs not browser
+
+console.log(3 + 
+    3); // code readabilty should be high
+
+console.log("aman");
+
+let name = "Aman";
+let age = 19;
+let isLoggedin = true;
+let state;
+
+// number => 2 to the power 53
+// big int
+// string => ""
+//boolean => true or false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+// object
+
+console.log(typeof null); // null is an object
+                         // undefined is undefined
